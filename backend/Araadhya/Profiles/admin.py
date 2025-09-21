9a39322efebe5bbe4321a622082b33258d049061
Vansh Verma <goluvansh93@gmail.com>
2025-09-21 17:49:25 +0530

    Delete backend/Araadhya directory

--- backend/Araadhya/Profiles/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from Profiles.models import Profile, Task, Skills
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Task)
-admin.site.register(Skills)
--- backend/Araadhya/Users/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-
-class PublicUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ['id', 'username']
